1455d0f36a8fdc298f8af8e523bf7cb1077df314
Alberto Miranda <amiranda@elementum.com>
2017-10-13 13:34:46 -0300

    Local playback put under an ENV var; added delayed instabuddy playback to wait for WS connection.

--- audio-player.js
+++ audio-player.js
@@ -9,8 +9,12 @@ function play (button) {
   const buttonData = audioMap[button]
   if (!buttonData) return
 
+  // Broadcast playback to InstaBuddy.
   console.log(`[ ${button} ] PLAYING ${buttonData.file}`)
   instabuddy.play(buttonData)
+
+  // Local playback.
+  if (!process.env.PLAY_LOCAL) return
   player.play(`${audioPath}/${buttonData.file}`, (err) => {
     if (err) console.log('AUDIO ERR:', err)
   })
--- instabuddy-connector.js
+++ instabuddy-connector.js
@@ -2,12 +2,35 @@ const WebSocket = require('ws')
 const ws = new WebSocket('wss://instabuddy.herokuapp.com')
 
 class InstabuddyConnector {
+  constructor () {
+    this.ready = false
+    this.onReadyCallback = null
+
+    ws.on('open', () => {
+      this.ready = true
+      if (this.onReadyCallback) {
+        this.onReadyCallback()
+      }
+    })
+  }
+
   onReady (callback) {
     if (typeof callback !== 'function') return
-    ws.on('open', callback)
+    this.onReadyCallback = callback
+    if (this.ready) {
+      callback()
+    }
   }
 
   play ({channel, id, src}) {
+    if (!this.ready) {
+      console.log('Delayed playback, waiting for WS to connect...')
+      setTimeout(() => {
+        this.play({channel, id, src})
+        return
+      }, 250)
+    }
+
     const message = {
       type: 'play',
       data: { channel, id, src }
