abeb77a9b685030850f877cfe0a73426822fe1af
Alberto Miranda <amiranda@elementum.com>
2017-10-13 15:54:54 -0300

    Waiting for devices.

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const ds = require('dualshock')
 const player = require('./audio-player')
 
-const devices = ds.getDevices('ds3')
+const waitMs = 5000
 
-if (!devices.length) {
-  console.log('NO DEVICES CONNECTED.')
-  process.exit()
-}
-console.log('DEVICES:', devices)
+function onDevicesFound (callback) {
+  const devices = ds.getDevices('ds3')
+  if (!devices.length) {
+    // Keep looking for devices.
+    setTimeout(() => {
+      onDevicesFound(callback)
+    }, waitMs)
+    return
+  }
 
-const device = devices[0]
-const gamepad = ds.open(device, {smoothAnalog:10, smoothMotion:15, joyDeadband:4, moveDeadband:4})
-
-gamepad.ondigital = (button, value) => {
-  // console.log(`BUTTON ${button} = ${value}`)
-  if (value) player(button)
-}
-
-gamepad.onanalog = (axis, value) => {
-  // console.log(`AXIS ${axis} = ${value}`)
+  console.log('DEVICES:', devices)
+  callback(devices)
 }
 
+console.log('Looking for devices...')
+onDevicesFound((devices) => {
+  setEvents(devices)
+})
+
+function setEvents (devices) {
+  const device = devices[0] // Support just one device at a time right now.
+  const gamepad = ds.open(device, {smoothAnalog:10, smoothMotion:15, joyDeadband:4, moveDeadband:4})
+
+  gamepad.ondigital = (button, value) => {
+    // console.log(`BUTTON ${button} = ${value}`)
+    if (value) player(button)
+  }
+
+  gamepad.onanalog = (axis, value) => {
+    // console.log(`AXIS ${axis} = ${value}`)
+  }
+}
