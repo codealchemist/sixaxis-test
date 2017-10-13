06f3bdf65ea26881d72ef36a0302fa500ecaea95
Alberto Miranda <codealchemist@gmail.com>
2017-10-13 01:55:57 +0000

    Adding test audio.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const path = require('path')
 const ds = require('dualshock')
+const player = require('play-sound')(opts = {})
+
 const devices = ds.getDevices('ds3')
+const audioPath = path.join(__dirname, 'audio')
 
 if (!devices.length) {
 	console.log('NO DEVICES CONNECTED.')
@@ -12,6 +16,9 @@ const gamepad = ds.open(device, {smoothAnalog:10, smoothMotion:15, joyDeadband:4
 
 gamepad.ondigital = (button, value) => {
 	console.log(`BUTTON ${button} = ${value}`)
+	player.play(`${audioPath}/nivel.webm`, (err) => {
+		console.log('AUDIO ERR:', err)
+	})
 }
 
 gamepad.onanalog = (axis, value) => {
